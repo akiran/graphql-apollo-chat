42f9b995c4b823af2a59e7f76f865e7430466fa6
kiran <kiran@neostack.com>
2017-12-09 13:26:04 +0530

    cleanup

--- src/server/index.js
+++ src/server/index.js
@@ -2,13 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import { graphqlExpress, graphiqlExpress } from "apollo-server-express";
 import schema from "./schema";
-const PORT = 8000;
 import pubsub from "./pubsub";
 import setupFunctions from "./setupFunctions";
 import { execute, subscribe } from "graphql";
 import { createServer } from "http";
-import { PubSub, SubscriptionManager } from "graphql-subscriptions";
 import { SubscriptionServer } from "subscriptions-transport-ws";
+const PORT = 8000;
 
 const app = express();
 app.use("/graphql", bodyParser.json(), graphqlExpress({ schema }));
@@ -26,6 +25,5 @@ app.use(
     endpointURL: "/graphql"
   })
 );
-// bodyParser is needed just for POST.
 
 server.listen(PORT);
